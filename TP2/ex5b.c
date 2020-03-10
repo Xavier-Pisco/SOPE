ed6ffc456be5b066d10791bfffd708bcb3ccf592
Xavier-Pisco <up201806134@fe.up.pt>
2020-03-10 23:35:33 +0000

    Finished 2nd TP

---
+++ TP2/ex5b.c
@@ -0,0 +1,16 @@
+#include "termios.h"
+#include "unistd.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+int main(void)
+{
+    int fd;
+    char *text1="CCCCC";
+    char *text2="DDDDD";
+    fd = open("f1.txt", O_WRONLY|O_SYNC,0600);
+    write(fd,text1,5);
+    write(fd,text2,5);
+    close(fd);
+    return 0;
+}
